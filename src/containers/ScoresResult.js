edaf0d522fcefaf77668b5fba3fd04c1fc7a6197
laushinka <laurie.malau@gmail.com>
2016-08-21 18:36:10 -0400

    Add functions for trial modal

--- src/containers/ScoresResult.js
+++ src/containers/ScoresResult.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
+import {openModal} from  '../actions/modals'
 import PreferencesSlidersContainer from '../containers/preferencesSlidersContainer'
 import TotalScoreBox from '../components/totalScoreBox'
 import ScoreGraph from '../components/scoreGraph'
 import BarGraph from '../components/barGraph'
 import radarChart from '../components/radarChart'
 import barChart from '../components/barChart'
+import Modal from '../components/modal'
 
 const ScoresResult = class extends Component {
   render() {
@@ -18,7 +20,7 @@ const ScoresResult = class extends Component {
       "bikes": "Transportation",
       "parks": "Parks"
     }
-    var scores = this.props.scores.scores
+    var scores = this.props.scores
     var preferences = this.props.preferences
     var labels = Object.keys(scores);
     var totalWeight = 0
@@ -36,38 +38,55 @@ const ScoresResult = class extends Component {
     })
     totalScore = Math.floor(totalScore / totalWeight)
     return(
-      <div className="container-fluid">
+      <div>
+        {this.renderModal('accidents')}
+        <div className="container-fluid">
+          <button onClick={this.handleAccidentCategoryClick.bind(this)}>Simulate click on accidents</button>
+          <div className="row" id="chartsBox">
+            <TotalScoreBox className="col-md-12" totalScore={totalScore} />
+            <div className='col-md-4'>
+              <PreferencesSlidersContainer />
+            </div>
+            <div className="col-md-4" id="radarChartBox">
+              <ScoreGraph scores={scores} keys={labels} radarChart={radarChart} scoreData={scoreData}/>
+            </div>
 
-        <div className="row" id="chartsBox">
-          <TotalScoreBox className="col-md-12" totalScore={totalScore} />
-          <div className='col-md-4'>
-            <PreferencesSlidersContainer />
-          </div>
-          <div className="col-md-4" id="radarChartBox">
-            <ScoreGraph scores={scores} keys={labels} radarChart={radarChart} scoreData={scoreData}/>
-          </div>
-
-          <div className="col-md-4" id="barChartBox">
-            <BarGraph keys={labels} scores={this.props.scores.attributes} averages={this.props.scores.averages} barChart={barChart}/>
+            <div className="col-md-4" id="barChartBox">
+              <BarGraph keys={labels} scores={this.props.attributes} averages={this.props.averages} barChart={barChart}/>
+            </div>
           </div>
         </div>
-      </div>
+        </div>
+
     )
+  }
+
+  renderModal(category) {
+    if (this.props.modals === `DETAILS_MODAL_${category}`) {
+      return (<Modal scores={this.props.scores}  />)
+
+    }
+  }
+
+  handleAccidentCategoryClick() {
+    // pass in different category
+    this.props.openModal('DETAILS_MODAL', 'accidents')
   }
 };
 
 function mapStateToProps(state) {
+  //debugger
   return {
-    scores: state.scores.data,
-    averages: state.scores.data,
-    attributes: state.scores.data,
-    preferences: state.preferences.preferences
-
+    scores: state.scores.scores.data,
+    averages: state.scores.averages.data,
+    attributes: state.scores.attributes,
+    preferences: state.preferences.preferences,
+    modals: state.modals
   }
 }
 
-// function mapDispatchToProps(dispatch){
-//   return bindActionCreators({fetchScores}, dispatch)
-// }
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({openModal}, dispatch)
+}
 
-export default connect(mapStateToProps)(ScoresResult)
+export default connect(mapStateToProps, mapDispatchToProps)(ScoresResult)
