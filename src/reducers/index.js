0d161e7fd8445f58bba9eecb981eed92bda8cc86
laushinka <laurie.malau@gmail.com>
2016-08-21 18:36:37 -0400

    Add modalsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import searchReducer from './searchReducer'
 import scoresReducer from './scoresReducer'
 import preferencesReducer from './preferencesReducer'
+import modalsReducer from './modalsReducer'
 
 // Mapping of state
 const rootReducer = combineReducers({
   address: searchReducer,
   preferences: preferencesReducer,
-  scores: scoresReducer
+  scores: scoresReducer,
+  modals: modalsReducer
 });
 
 export default rootReducer;
