8a78cc48817cde54b0d7df54ddb5a5d1f034356b
laushinka <laurie.malau@gmail.com>
2016-08-21 18:37:25 -0400

    Create tryout modal component

---
+++ src/components/modal.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <div style={this.style()}>
+      {/* {this.props.scores} */}
+      </div>
+    )
+  }
+
+  style() {
+    return {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      background: 'red',
+      zIndex: 1
+    }
+  }
+}
